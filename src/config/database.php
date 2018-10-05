8d65e78f5668f60986a093b6ea211fea01698c1e
masuoshinichiro <masuo@wh-plus.com>
2018-10-05 15:29:35 +0900

    5-1,2 DBの準備　DBクラスの利用

--- src/config/database.php
+++ src/config/database.php
@@ -41,7 +41,8 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            //'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => 'mysql',
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),

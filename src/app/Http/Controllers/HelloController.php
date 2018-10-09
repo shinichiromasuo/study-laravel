95d714cfbf1a4b3639e6a4c7a70f8e34890b3912
masuoshinichiro <masuo@wh-plus.com>
2018-10-09 13:06:11 +0900

    5-4 マイグレーションとシーティング

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -12,7 +12,7 @@ class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $items = DB::select('select * from people');
+        $items = DB::table('people')->orderBy('id', 'asc')->get();
         return view('hello.index', ['items' => $items]);
     }
     public function post(Request $request)
@@ -32,17 +32,16 @@ class HelloController extends Controller
             'mail' => $request->mail,
             'age' => $request->age,
         ];
-        DB::insert('insert into people (name, mail, age) values
-            (:name, :mail, :age)', $param);
+        DB::table('people')->insert($param);
         return redirect('/hello');
     }
 
     public function edit(Request $request)
     {
 
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.edit', ['form' => $item[0]]);
+        $item = DB::table('people')
+            ->where('id', $request->id)->first();
+        return view('hello.edit', ['form' => $item]);
     }
 
     public function update(Request $request)
@@ -53,15 +52,17 @@ class HelloController extends Controller
             'mail' => $request->mail,
             'age' => $request->age,
         ];
-        DB::update('update people set name = :name, mail = :mail, age = :age where id = :id', $param);
+        DB::table('people')
+            ->where('id', $request->id)
+            ->update($param);
         return redirect('/hello');
     }
 
     public function del(Request $request)
     {
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.del', ['form' => $item[0]]);
+        $item = DB::table('people')
+            ->where('id', $request->id)->first();
+        return view('hello.del', ['form' => $item]);
     }
 
     public function remove(Request $request)
@@ -71,4 +72,13 @@ class HelloController extends Controller
         return redirect('/hello');
     }
 
+    public function show(Request $request)
+    {
+        $page = $request->page;
+        $items = DB::table('people')
+            ->offset($page * 3)
+            ->liit(3)
+            ->get();
+        return view('hello.show', ['items' => $items]);
+    }
 }

0b491af25a6192afa87c704375eae1e970531671
masuoshinichiro <masuo@wh-plus.com>
2018-10-11 11:22:29 +0900

    6-3 モデルの保存、更新、削除

--- src/app/Person.php
+++ src/app/Person.php
@@ -10,27 +10,14 @@ class Person extends Model
 {
     public function getData()
     {
-        return $this->id . ': ' . $this->name . '(' . $this->age . ')';
+        return $this->id . ':' . $this->name . ' (' . $this->age . ')';
     }
 
-    public function scopeNameEqual($query, $str)
-    {
-        return $query->where('name', $str);
-    }
+    protected $guarded = array('id');
 
-    public function scopeAgeGreaterThan($query, $n)
-    {
-        return $query->where('age', '>=', $n);
-    }
-
-    public function scopeAgeLessThan($query, $n)
-    {
-        return $query->where('age', '<=', $n);
-    }
-
-    protected static function boot()
-    {
-        parent::boot();
-        static::addGlobalScope(new ScopePerson);
-    }
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
 }

e3eb1ab109186e94cc5d91d1108063d6f4399a9c
masuoshinichiro <masuo@wh-plus.com>
2018-10-12 15:54:24 +0900

    7-1 リソースコントローラとRESTful

--- src/routes/web.php
+++ src/routes/web.php
@@ -48,3 +48,7 @@ Route::get('board', 'BoardController@index');
 
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create');
+
+Route::resource('rest', 'RestappController');
+
+Route::get('hello/rest', 'HelloController@rest');
